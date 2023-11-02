7c9909b2e6f3d24abbd61605231cb2d9232ee97d
nachomglz <nachopad3@gmail.com>
2023-11-02 13:39:36 +0100

    create and retrieve urls from db

--- src/controllers/url.ts
+++ src/controllers/url.ts
@@ -1,4 +1,4 @@
-import type { Request, Response } from '../utils/types'
+import type { Request, Response, Url } from '../utils/types'
 import db from '../utils/db'
 
 export default class UrlController {
@@ -6,11 +6,60 @@ export default class UrlController {
         return new RegExp('^(http|https)://', 'i').test(url)
     }
 
-    public static getUrl(req: Request, res: Response) {
-        return res.send({
-            ok: true,
-            data: []
+    static generateSuggestion(times_executed: number = 0): string | null {
+        const suggestion = Math.random().toString(36).substring(2, 15)
+        // if there's an error in the query try again 5 times, if its still failing return null
+        db.get<Url>('SELECT * FROM url WHERE shortened_url = ?', [suggestion], (err, row) => {
+            if (err) {
+                console.error(`[ERROR] ~ Checking suggestion: ${err}`)
+                if (times_executed < 5) {
+                    return UrlController.generateSuggestion(times_executed + 1)
+                } else {
+                    return null
+                }
+            }
+
+            if (row && row.id > 0) {
+                return UrlController.generateSuggestion()
+            }
         })
+        return suggestion
+    }
+
+    public static getUrl(req: Request, res: Response) {
+        // get the shortened url from the request if one, if not return all the urls
+        const { shortened_url } = req.query
+        if (shortened_url) {
+            const url = db.get<Url>('SELECT * FROM url WHERE shortened_url = ?', shortened_url)
+            if (url) {
+                return res.send({
+                    ok: true,
+                    data: url
+                })
+            } else {
+                return res.status(404).send({
+                    ok: false,
+                    data: {
+                        message: 'Url not found'
+                    }
+                })
+            }
+        } else {
+            const urls = db.all<Url>('SELECT * FROM url', (error, rows) => {
+                if (error) {
+                    return res.send({
+                        ok: false,
+                        data: {
+                            message: 'Something went wrong while fetching the urls'
+                        }
+                    })
+                }
+                return res.send({
+                    ok: true,
+                    data: rows
+                })
+            })
+        }
     }
 
     public static createUrl(req: Request<{ url: string, suggestion: string }>, res: Response) {
@@ -26,7 +75,7 @@ export default class UrlController {
             })
         }
 
-        // check if the url is valid
+        // check if url is correctly formatted
         if (!UrlController.isValidUrl(url)) {
             return res.status(400).send({
                 ok: false,
@@ -36,13 +85,27 @@ export default class UrlController {
             })
         }
 
-        // the suggestion is optional, if it's not provided, generate a random string
-        // the suggestion must be unique and at least 3 characters long with a limit of 20 characters
         const suggestionRegex = /^[a-zA-Z0-9]{3,20}$/
-        let finalSuggestion = suggestion
+        let finalSuggestion: string | null = suggestion
         if (!suggestion || !suggestionRegex.test(suggestion)) {
-            finalSuggestion = Math.random().toString(36).substring(2, 15)
+            finalSuggestion = UrlController.generateSuggestion()
+            if (finalSuggestion === null) {
+                return res.status(500).send({
+                    ok: false,
+                    data: {
+                        message: 'Something went wrong while generating the suggestion'
+                    }
+                })
+            }
         }
+
+        // insert the url into the db
+        db.run(
+            'INSERT INTO url (expiration_date, original_url, shortened_url) VALUES (?, ?, ?)',
+            new Date(),
+            url,
+            finalSuggestion
+        )
 
         res.send({
             ok: true,
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,14 +5,18 @@ export interface Request<T = any> extends ExpressRequest {
     body: T
 }
 
-// Create type for the object that is sent in the response body.
 export type ResponseBody<T = any> = {
     ok: boolean,
     data: T
 }
 
-
-// Create a custom Response type that extends the ExpressResponse type in which i can set the type for the object that is sent in the response body.
 export interface Response<T = any> extends ExpressResponse {
     send: (body?: ResponseBody<T>) => any
+}
+
+export interface Url {
+    id: number,
+    expiration_date: Date,
+    original_url: string,
+    shortened_url: string
 }
